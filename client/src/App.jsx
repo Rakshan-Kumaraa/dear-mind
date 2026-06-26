96b090d7e407becef01f2ccc9412c01fa25e66fe
Rakshan-Kumaraa <rakshankumaraa@gmail.com>
2026-06-26 13:18:54 +0530

    Fixed JSX comment error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -187,7 +187,7 @@ function App() {
           isOpen={isSidebarOpen} 
           onClose={() => setIsSidebarOpen(false)} 
           onLogout={handleLogout}
-          entries={entries} {/* <--- Add this exact line right here! */}
+          entries={entries}
         />
         
       </div>
